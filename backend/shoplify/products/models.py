b7d6141f64693180a42cf979a224f76483dec435
mahbobhasan <mahbobhasan66@gmail.com>
2025-09-06 09:33:14 +0600

    product and category models are created

--- backend/shoplify/products/models.py
+++ backend/shoplify/products/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_id=models.AutoField(primary_key=True)
+    category_name=models.CharField(max_length=30)
 class Product(models.Model):
     product_id=models.AutoField(primary_key=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
     product_name=models.CharField(max_length=50)
     unit_price=models.DecimalField(max_digits=10,decimal_places=2)
     quantity=models.PositiveIntegerField()
@@ -10,3 +15,5 @@ class Product(models.Model):
     
     def __str__(self):
         return str(self.name)
+class Sales(models.Model):
+    pass
