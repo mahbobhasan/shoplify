b52ad5f4dec4921fb47dc6cad1ec7b8b367c5787
mahbobhasan <mahbobhasan66@gmail.com>
2025-09-08 10:01:53 +0600

    complete the apis of Product

--- backend/shoplify/products/models.py
+++ backend/shoplify/products/models.py
@@ -3,17 +3,22 @@ from django.db import models
 # Create your models here. ok
 
 class Category(models.Model):
+    class Meta:
+        verbose_name="catagorie"
     category_id=models.AutoField(primary_key=True)
     category_name=models.CharField(max_length=30)
-class Product(models.Model):
+    def __str__(self):
+        return f"{self.category_id} --> {self.category_name} "
+class Product(models.Model):    
     product_id=models.AutoField(primary_key=True)
     category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
     product_name=models.CharField(max_length=50)
     unit_price=models.DecimalField(max_digits=10,decimal_places=2)
     quantity=models.PositiveIntegerField()
     cost_price=models.DecimalField(max_digits=10,decimal_places=2)
+    image=models.ImageField(upload_to="products/",null=True,blank=True)
     
     def __str__(self):
-        return str(self.name)
+        return f"{self.product_id} --> {self.product_name} "
 class Sales(models.Model):
     pass
--- backend/shoplify/shoplify/urls.py
+++ backend/shoplify/shoplify/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include("accounts.urls"), name="accounts"),
     path('products/',include("products.urls"),name="products"),
-    path('orders/',include("orders.urls"),name="orders")
+    path('orders/',include("orders.urls"),name="orders"),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
